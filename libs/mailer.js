a58f282cef12cb437eaf2e9cabc3c105551c6460
SubeljEtra <jan.subelj@kolektor.com>
2020-02-03 12:55:56 +0100

    added sendgrid mailer, added new ip watcher

--- libs/mailer.js
+++ libs/mailer.js
@@ -1,6 +1,10 @@
 if(process.env.PRODUCTION == "false") require('dotenv').config()
 const nodemailer = require('nodemailer');
 const publicIp = require("public-ip");
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+var http = require('http');
 
 
 const transporter = nodemailer.createTransport({
@@ -18,25 +22,45 @@ let mailOptions = {
 	//html: '<p> Ip was changed! This is the new ip: </p>'
 }
 
+const schedule=require('node-schedule');
+
+
 
 module.exports.send_mail_with_ip = (ip, callback) => {
+	console.log("Api key",process.env.SENDGRID_API_KEY);
 	if (ip){
+		
 		mailOptions.html = `<h1>Your server's IP has changed! </h1> <h3>The new IP is: <b>${ip}</b></h3><p>Sent from <a href="https://github.com/JSubelj/freenom-dns-updater">freenom-dns-updater</a></p>`
-		transporter.sendMail(mailOptions, (err,info)=>{
-			if(err) console.log(err);
-			else console.log(info);
-			if (callback) callback(err,info);
-		})
+		console.log("Sending mail");
+		sgMail.send(mailOptions,(err)=>{
+					transporter.sendMail(mailOptions, (err,info)=>{
+					if(err) console.log(err);
+					else console.log(info);
+					if (callback) callback(err,info);
+					})
+					
+				});
 	}else{
-		publicIp.v4().then(ip => {
-			mailOptions.html = `<h1>Your server's IP has changed! </h1> <h3>The new IP is: <b>${ip}</b></h3><p>Sent from <a href="https://github.com/JSubelj/freenom-dns-updater">freenom-dns-updater</a></p>`
-			transporter.sendMail(mailOptions, (err,info)=>{
-				if(err) console.log(err);
-				else console.log(info);
-				if (callback) callback(err,info);
-			})
-		})
+		http.get({'host': 'api.ipify.org', 'port': 80, 'path': '/'}, function(resp) {
+			resp.on('data', function(ip) {
+				console.log("Sending mail");
+			
+				mailOptions.html = `<h1>Your server's IP has changed! </h1> <h3>The new IP is: <b>${ip}</b></h3><p>Sent from <a href="https://github.com/JSubelj/freenom-dns-updater">freenom-dns-updater</a></p>`
+				sgMail.send(mailOptions,(err)=>{
+					transporter.sendMail(mailOptions, (err,info)=>{
+					if(err) console.log(err);
+					else console.log(info);
+					if (callback) callback(err,info);
+					})
+					
+				});
+				
+			});
+		});
 	}
 }
 
 
+schedule.scheduleJob({hour: 00, minute:00}, () => { 
+	module.exports.send_mail_with_ip();
+})
